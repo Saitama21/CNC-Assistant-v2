f541e1a8c9b9df85a13ac71c42402f551cb31565
Saitama21 <62865879+Saitama21@users.noreply.github.com>
2026-08-08 03:56:07 +0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -91,10 +91,6 @@ app.use(
   })
 );
 
-app.get("*", (_req, res) => {
+app.use((_req, res) => {
   res.sendFile(path.join(publicDir, "index.html"));
-});
-
-app.listen(port, "0.0.0.0", () => {
-  console.log(`CNC Assistant listening on port ${port}`);
 });
